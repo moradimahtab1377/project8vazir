771d19f910eb50bea36ae70cfa82221a7471dd2d
amir.pc <moradimahtab1377@gmail.com>
2025-05-11 22:28:36 +0330

    Create a parent selection function to scode.py

--- scode.py
+++ scode.py
@@ -1,3 +1,8 @@
 # تابع برای ایجاد یک کروموزوم تصادفی
     def random_chromosome(self):
         return random.sample(range(self.n), self.n)  
+    def selection(self):
+        weighted_population = [(self.fitness(chromosome), chromosome) for chromosome in self.population]
+        weighted_population.sort(reverse=True)
+        selected = [chromosome for fitness, chromosome in weighted_population[:self.population_size // 2]]
+        return selected
