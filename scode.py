151d2f481f9fbd80a52c480833dd1e2ab7163c16
moradimahtab1377 <moradimahtab1377@gmail.com>
2025-05-12 11:30:01 +0330

    Update scode.py in github &add rasm algoritm

--- scode.py
+++ scode.py
@@ -6,3 +6,10 @@
         weighted_population.sort(reverse=True)
         selected = [chromosome for fitness, chromosome in weighted_population[:self.population_size // 2]]
         return selected
+
+ # تابع اجرای الگوریتم
+    def run(self):
+        for generation in range(self.generations):
+            self.next_generation()
+            best_fit = max(self.population, key=lambda chrom: self.fitness(chrom))
+            if self.fitness(best_fit) == self.n * (self.n - 1) // 2:
